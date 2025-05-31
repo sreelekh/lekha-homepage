8f7873717e051267d39e2f3f3df641ca3e9fef03
Sreelekha Guggilam <33463723+sreelekh@users.noreply.github.com>
2025-05-30 22:32:44 -0500

    Test V3

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,19 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [react(), tailwind(), sitemap()],
-    site: template.website_url,
-    base: template.base,
+    site: 'https://sreelekh.github.io/lekha-homepage/',
+    // base: '/REPOSITORY-NAME',  // Only if not using USERNAME.github.io
+    // ... rest of config
+    integrations: [
+        mdx(),
+        tailwind({
+            applyBaseStyles: false,
+        }),
+    ],
+    markdown: {
+        shikiConfig: {
+            theme: 'github-dark-dimmed',
+            wrap: true,
+        },
+    },
 });
